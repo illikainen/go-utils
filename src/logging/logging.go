300097db03c39f99a6764479c8c0fe209bc5b250
Hans Jerry Illikainen <hji@dyntopia.com>
2024-07-02 17:26:58 +0000

    logging: add SanitizedJSONFormatter{} and GetField()

--- src/logging/logging.go
+++ src/logging/logging.go
@@ -19,6 +19,18 @@ func WithSuppress(fn func() error) error {
 	return err
 }
 
+type SanitizedJSONFormatter struct {
+}
+
+func (f *SanitizedJSONFormatter) Format(e *log.Entry) ([]byte, error) {
+	formatter := log.JSONFormatter{}
+	out, err := formatter.Format(e)
+	if err != nil {
+		return nil, err
+	}
+	return stringx.Sanitize(string(out)), nil
+}
+
 type SanitizedTextFormatter struct {
 }
 
@@ -41,4 +53,18 @@ func (f *SanitizedTextFormatter) Format(entry *log.Entry) ([]byte, error) {
 	return lo.FlatMap(stringx.SplitLines(entry.Message), func(line string, _ int) []byte {
 		return []byte(fmt.Sprintf("%-14s | %s\n", level, stringx.Sanitize(line)))
 	}), nil
+}
+
+func GetField(fields log.Fields, key string, fallback string) string {
+	value, ok := fields[key]
+	if !ok {
+		return fallback
+	}
+
+	str, ok := value.(string)
+	if !ok {
+		return fallback
+	}
+
+	return str
 }
