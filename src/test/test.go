39e41a2977639190974f90a86e178c6c4a838d3d
Hans Jerry Illikainen <hji@dyntopia.com>
2025-07-21 14:39:04 +0000

    test: add AssertErr()

--- src/test/test.go
+++ src/test/test.go
@@ -3,6 +3,8 @@ package test
 import (
 	"reflect"
 	"testing"
+
+	"github.com/pkg/errors"
 )
 
 func AssertEq(t *testing.T, x any, y any) {
@@ -18,5 +20,13 @@ func AssertNe(t *testing.T, x any, y any) {
 
 	if reflect.DeepEqual(x, y) {
 		t.Fatalf("%v != %v", x, y)
+	}
+}
+
+func AssertErr(t *testing.T, actual error, expected error) {
+	t.Helper()
+
+	if !errors.Is(actual, expected) {
+		t.Fatalf("%v != %v", actual, expected)
 	}
 }
